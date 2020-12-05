483b718e8ab98e5c0988114a462fad22a3f6ea91
Maxime Graindor <maxime.graindor@hotmail.com>
2020-12-05 21:30:12 +0100

    refactoring the code

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,34 +1,19 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="fr">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @hasSection('title')
+        <title>Accueil - Marché des Gourmets</title>
 
-            <title>@yield('title') - {{ config('app.name') }}</title>
-        @else
-            <title>{{ config('app.name') }}</title>
-        @endif
-
-        <!-- Favicon -->
-		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css')) }}">
         @livewireStyles
 
-        <!-- Scripts -->
-        <script src="{{ url(mix('js/app.js')) }}" defer></script>
+        <script src="{{ asset('js/app.js')) }}" defer></script>
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
 
     <body>
-        @yield('body')
+        @yield('content')
 
         @livewireScripts
     </body>
