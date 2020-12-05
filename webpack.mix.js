9fd811ceba39b65b2d9faf953b22006d4d52c872
Maxime Graindor <maxime.graindor@hotmail.com>
2020-12-05 21:06:44 +0100

    Remove Tailwind from Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,6 @@ require("laravel-mix-tailwind");
 
 mix.js("resources/js/app.js", "public/js/app.js")
     .sass("resources/sass/app.scss", "public/css/app.css")
-    .tailwind("./tailwind.config.js")
     .sourceMaps();
 
 if (mix.inProduction()) {
