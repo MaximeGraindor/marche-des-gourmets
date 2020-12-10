7e075f47c0cb2e8349ca5c2828221cd06b12d76d
Maxime Graindor <maxime.graindor@hotmail.com>
2020-12-10 14:33:47 +0100

    Added social meta card for Facebook

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,6 +11,16 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
 
         <link rel="icon" type="image/png" href="/favicon.png">
+
+        {{-- Social Meta --}}
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ URL::current() }}">
+        <meta property="og:title" content="@yield('title')">
+        <meta property="og:image" content="https://example.com/favicon.png">
+        <meta property="og:description" content=""> //TODO Add description of each page
+        <meta property="og:site_name" content="Marché des Gourmets">
+        <meta property="og:locale" content="fr">
+
     </head>
 
     <body>
