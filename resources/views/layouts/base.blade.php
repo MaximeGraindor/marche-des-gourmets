98c356afd6972f1173fb746122fa13ff6098efa4
Maxime Graindor <maxime.graindor@hotmail.com>
2020-12-12 20:38:02 +0100

    Global update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,9 +7,9 @@
 
         <meta name="description" content="Le Marché des Gourmets est un événement annuel organisé par le Rotary Club de Flémalle sur le prestigieux site de l'abbaye de la Paix-Dieu afin d'obtenir des fonds pour aider les plus démunis." >
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
         @livewireStyles
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('/js/app.js') }}" defer></script>
         <link rel="icon" type="image/png" href="/favicon.png">
 
         {{-- Social Meta --}}
