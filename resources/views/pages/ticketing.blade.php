de27d77ffb4589b653b2a3e0bb915b50953c46b5
Maxime Graindor <maxime.graindor@hotmail.com>
2020-12-14 20:04:37 +0100

    Updated ticketing page

--- resources/views/pages/ticketing.blade.php
+++ resources/views/pages/ticketing.blade.php
@@ -5,5 +5,19 @@
         Billeterie - Marché des Gourmets
     </h1>
     <x-header/>
+
+    <main class="billeterie">
+        <h2 class="billeterie-title" role="heading" aria-level="2">
+            Achetez un billet
+        </h2>
+
+        <div class="billeterie-alert">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo inventore assumenda id accusantium delectus hic voluptates, aperiam, commodi nobis nemo possimus voluptas necessitatibus expedita. Quos quisquam voluptas rem exercitationem eveniet. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio modi laboriosam perferendis dolores quaerat. Amet, ad dolores quos in ullam placeat, molestiae rerum eum modi temporibus libero earum et maiores!
+            </p>
+        </div>
+
+    </main>
+
     <x-footer/>
 @endsection
