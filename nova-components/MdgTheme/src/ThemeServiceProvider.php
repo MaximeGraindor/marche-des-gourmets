d2851d22de8d8de56735d7a50a2ef07fa9ff698e
Maxime Graindor <maxime.graindor@hotmail.com>
2020-12-29 23:19:45 +0100

    created custom theme for nova

---
+++ nova-components/MdgTheme/src/ThemeServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Theme\MdgTheme;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::theme(asset('/theme/mdg-theme/theme.css'));
+        });
+
+        $this->publishes([
+            __DIR__.'/../resources/css' => public_path('theme/mdg-theme'),
+        ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
